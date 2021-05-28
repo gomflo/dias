adc31aca9aaf4c726ffbd6b61614442047e1e9ea
Hugo <gomflo@gmail.com>
2021-05-28 15:17:58 -0500

    logo con emojis

--- pages/[date].js
+++ pages/[date].js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Countdown from '@/components/Countdown/Countdown'
 
@@ -8,6 +9,10 @@ export default function Day() {
 
   return (
     <div className="min-h-screen flex justify-center items-center p-10">
+
+      <Link href="/">
+        <a className="text-xl font-semibold absolute top-0 mt-5">📆😀</a>
+      </Link>
       <Countdown propDate={routerDate} />
     </div>
   )
